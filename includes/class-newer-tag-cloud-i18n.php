a366e6ee542df169f999144752a7af231717260e
Dan Pock <dpock32509@gmail.com>
2017-02-17 21:36:27 -0500

    initial commit: WP Plugin Boiler
    http://wppb.io/

---
+++ includes/class-newer-tag-cloud-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://github.com/mallardduck
+ * @since      1.0.0
+ *
+ * @package    Newer_Tag_Cloud
+ * @subpackage Newer_Tag_Cloud/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Newer_Tag_Cloud
+ * @subpackage Newer_Tag_Cloud/includes
+ * @author     Dan Pock (Liquid Web) <dpock@liquidweb.com>
+ */
+class Newer_Tag_Cloud_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'newer-tag-cloud',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
