252fb543135c0d9a8f3c7705c2f0428eda990a67
Dan Pock <dpock32509@gmail.com>
2017-04-16 19:51:40 -0400

    improve views

--- admin/partials/newer-tag-cloud-instances-display.php
+++ admin/partials/newer-tag-cloud-instances-display.php
@@ -32,69 +32,68 @@
 
 <div>
     <form action="" method="post" name="<?php echo $pluginName ?>-instance">
-        <table class="form-table">
-            <tbody>
-                <h2>Widget Option</h2>
-                <tr valign="top">
-                    <th scope="row">Instance Widget Title:</th>
-                    <td><input type="text" id="<?php echo $pluginName ?>-instance-title" name="<?php echo $pluginName ?>-instance-title" value="<?php echo($instanceOptions['title']); ?>" /></td>
-                </tr>
-                <h2>General Option</h2>
-                <tr valign="top">
-                    <th scope="row">Tag Cloud Instance Name:</th>
-                    <td><input type="text" id="<?php echo $pluginName ?>-instancename" name="<?php echo $pluginName ?>-instancename" value="<?php echo($instanceName); ?>" size="<?php echo(strlen($instanceName)+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Max Tag Count:</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-max_count" value="<?php echo($instanceOptions['max_count']); ?>" size="<?php echo(strlen($instanceOptions['max_count'])+5); ?>" /></td>
-                </tr>
-                <h2>Style and Markup Option</h2>
-                <tr valign="top">
-                    <th scope="row">Largest Tag Font Size:</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-big_size" value="<?php echo($instanceOptions['big_size']); ?>" size="<?php echo(strlen($instanceOptions['big_size'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Smallest Tag Font Size:</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-small_size" value="<?php echo($instanceOptions['small_size']); ?>" size="<?php echo(strlen($instanceOptions['small_size'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Step Size (Font Size difference between tags):</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-step" value="<?php echo($instanceOptions['step']); ?>" size="<?php echo(strlen($instanceOptions['step'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Font Size Unit (CSS sizes e.g. px, pt, em, etc):?</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-size_unit" value="<?php echo($instanceOptions['size_unit']); ?>" size="<?php echo(strlen($instanceOptions['size_unit'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">HTML to Insert Before the Cloud:</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-html_before" value="<?php echo(htmlentities($instanceOptions['html_before'])); ?>" size="<?php echo(strlen($instanceOptions['html_before'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">HTML to Insert After the Cloud:</th>
-                    <td><input type="text" name="<?php echo $pluginName ?>-htmlafter" value="<?php echo(htmlentities($instanceOptions['html_after'])); ?>" size="<?php echo(strlen($instanceOptions['html_after'])+5); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Tag Entry Template:</th>
-                    <td><input type="text" id="<?php echo $pluginName ?>-entrylayout" name="<?php echo $pluginName ?>-entrylayout" value="<?php echo(htmlentities($instanceOptions['entry_layout'])); ?>" size="<?php echo(strlen($instanceOptions['entry_layout'])+5); ?>" onkeyup="update_example();" onkeydown="update_example();" /><br/><div id="<?php echo $pluginName ?>-entrylayout-example"></div></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Glue Character (the string inserted between tags):</th>
-                    <td><input type="text" id="<?php echo $pluginName ?>-glue" name="<?php echo $pluginName ?>-glue" value="<?php echo(htmlentities($instanceOptions['glue'])); ?>" size="<?php echo(strlen($instanceOptions['glue'])+5); ?>" /><input type="button" value="Use a Space" onClick="glueChar('%SPACE%')"/></td>
+        <ul class="form-table">
+                <h3>Widget Options</h3>
+                <li>
+                    <span class="label">Instance Widget Title:</span>
+                    <span class="input"><input type="text" id="<?php echo $pluginName ?>-instance-title" name="<?php echo $pluginName ?>-instance-title" value="<?php echo($instanceOptions['title']); ?>" /></span>
+                </li>
+                <h3>General Options</h3>
+                <li>
+                    <span class="label">Tag Cloud Instance Name:</span>
+                    <span class="input"><input type="text" id="<?php echo $pluginName ?>-instancename" name="<?php echo $pluginName ?>-instancename" value="<?php echo($instanceName); ?>" size="<?php echo(strlen($instanceName)+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">Max Tag Count:</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-max_count" value="<?php echo($instanceOptions['max_count']); ?>" size="<?php echo(strlen($instanceOptions['max_count'])+5); ?>" /></span>
+                </li>
+                <h3>Style and Markup Options</h3>
+                <li>
+                    <span class="label">Largest Tag Font Size:</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-big_size" value="<?php echo($instanceOptions['big_size']); ?>" size="<?php echo(strlen($instanceOptions['big_size'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">Smallest Tag Font Size:</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-small_size" value="<?php echo($instanceOptions['small_size']); ?>" size="<?php echo(strlen($instanceOptions['small_size'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">Step Size (Font Size difference between tags):</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-step" value="<?php echo($instanceOptions['step']); ?>" size="<?php echo(strlen($instanceOptions['step'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">Font Size Unit (CSS sizes e.g. px, pt, em, etc):</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-size_unit" value="<?php echo($instanceOptions['size_unit']); ?>" size="<?php echo(strlen($instanceOptions['size_unit'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">HTML to Insert Before the Cloud:</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-html_before" value="<?php echo(htmlentities($instanceOptions['html_before'])); ?>" size="<?php echo(strlen($instanceOptions['html_before'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">HTML to Insert After the Cloud:</span>
+                    <span class="input"><input type="text" name="<?php echo $pluginName ?>-htmlafter" value="<?php echo(htmlentities($instanceOptions['html_after'])); ?>" size="<?php echo(strlen($instanceOptions['html_after'])+5); ?>" /></span>
+                </li>
+                <li>
+                    <span class="label">Tag Entry Template:</span>
+                    <span class="input"><input type="text" id="<?php echo $pluginName ?>-entrylayout" name="<?php echo $pluginName ?>-entrylayout" value="<?php echo(htmlentities($instanceOptions['entry_layout'])); ?>" size="<?php echo(strlen($instanceOptions['entry_layout'])+5); ?>" onkeyup="update_example();" onkeydown="update_example();" /><br/><div id="<?php echo $pluginName ?>-entrylayout-example"></div></span>
+                </li>
+                <li>
+                    <span class="label">Glue Character (the string inserted between tags):</span>
+                    <span class="input"><input type="text" id="<?php echo $pluginName ?>-glue" name="<?php echo $pluginName ?>-glue" value="<?php echo(htmlentities($instanceOptions['glue'])); ?>" size="<?php echo(strlen($instanceOptions['glue'])+5); ?>" /><input type="button" value="Use a Space" onClick="glueChar('%SPACE%')"/></span>
                 <tr/>
-                <tr valign="top">
-                    <th scope="row">Cloud Tag Order:</th>
-                    <td><?php echo $options->create_selectfield($options->orderOptions, $instanceOptions['order'], $pluginName.'-order'); ?></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Category Filters:<br/>Hint: To disable category filtering, deselect all categories!</th>
-                    <td><?php $options->cat_filter_list($instanceOptions['cat_filter']); ?></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Tag Filters:</th>
-                    <td><input type="text" id="<?php echo $pluginName ?>-tag_filter" name="<?php echo $pluginName ?>-tag_filter" value="<?php echo(htmlentities($tagFilter)); ?>" size="<?php echo(strlen($tagFilter)+5); ?>" />* Comma seperated list</td>
-                </tr>
-            </tbody>
-        </table>
+                <h3>Order and Filter Options</h3>
+                <li>
+                    <span class="label">Cloud Tag Order:</span>
+                    <span class="input"><?php echo $options->create_selectfield($options->orderOptions, $instanceOptions['order'], $pluginName.'-order'); ?></span>
+                </li>
+                <li>
+                    <span class="label">Category Filters:<br/>Hint: To disable category filtering, deselect all categories!</span>
+                    <span class="input"><?php $options->cat_filter_list($instanceOptions['cat_filter']); ?></span>
+                </li>
+                <li>
+                    <span class="label">Tag Filters:</span>
+                    <span class="input"><input type="text" id="<?php echo $pluginName ?>-tag_filter" name="<?php echo $pluginName ?>-tag_filter" value="<?php echo(htmlentities($tagFilter)); ?>" size="<?php echo(strlen($tagFilter)+5); ?>" />* Comma seperated list</span>
+                </li>
+        </ul>
         <p class="submit">
             <input type="hidden" id="<?php echo $pluginName ?>-instance" name="<?php echo $pluginName ?>-instance" value="<?php echo $instanceToUse; ?>"/>
             <input type="hidden" name="<?php echo $pluginName ?>-originalinstancename" value="<?php echo $instanceName; ?>"/>
